597669e480dec5ab27149044d58e66d1093f91fd
Nicolas Aires <ni_colasaires@hotmail.com>
2026-02-24 21:47:38 -0300

    add flake8 linter, add requirements.txt, add ShortLink model with link_title, created_at, expires_at, original_link and click fields.

--- shortener/models.py
+++ shortener/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils import timezone
+from datetime import timedelta
 
-# Create your models here.
+class ShortLink(models.Model):
+    link_title = models.CharField(null=False, blank=False, max_length=100)
+    created_at = models.DateField(auto_now_add=True)
+    expires_at = models.DateField(default=lambda: timezone.now() + timedelta(days=5))
+    original_link = models.CharField(null=False, blank=False, max_length=200)
+    clicks = models.IntegerField(default=0)

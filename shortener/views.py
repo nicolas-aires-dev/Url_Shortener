5fc908a3d393beffc6229766d74dc57c4f256a32
Nicolas <ni_colasaires@hotmail.com>
2026-03-16 09:48:00 -0300

    Changed queryset xxx.all > xxx.all()

--- shortener/views.py
+++ shortener/views.py
@@ -1,8 +1,8 @@
 from rest_framework import generics
 from shortener.models import ShortLink
-from shortener.serializers import ShortLinkSerializer
+from shortener.serializer import ShortLinkSerializer
 
 
 class ShortLinkCreateListView(generics.ListCreateAPIView):
-    queryset = ShortLink.objects.all
+    queryset = ShortLink.objects.all()
     serializer_class = ShortLinkSerializer

2fb208f106e770f7447527b939e8cb08ba240514
Nicolas <ni_colasaires@hotmail.com>
2026-03-16 09:27:22 -0300

    Add serializer class at 'ShortLinkCreateListView' at views.py

--- shortener/views.py
+++ shortener/views.py
@@ -1,5 +1,8 @@
 from rest_framework import generics
 from shortener.models import ShortLink
+from shortener.serializers import ShortLinkSerializer
+
 
 class ShortLinkCreateListView(generics.ListCreateAPIView):
     queryset = ShortLink.objects.all
+    serializer_class = ShortLinkSerializer

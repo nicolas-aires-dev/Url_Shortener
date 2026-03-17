8f4ac618a8032f7fe56930ac8adb8c4114e5736f
Nicolas <ni_colasaires@hotmail.com>
2026-03-17 09:56:46 -0300

    feat: add services.py, algorithm who encode data to base62

--- shortener/services.py
+++ shortener/services.py
@@ -1,6 +1,16 @@
-#Services
+import string
 
 
-def generate_short_url(obj):
+BASE62 = string.digits + string.ascii_letters
 
+def base62_encode(data: bytes) -> str:
+    num = int.from_bytes(data, "big")
+    if num == 0:
+        return BASE62[0]
     
+    encoded = []
+    while num > 0:
+        num, rem = divmod(num, 62)
+        encoded.append(BASE62[rem])
+
+    return ''.join(reversed(encoded))

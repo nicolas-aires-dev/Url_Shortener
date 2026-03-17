039b57e1fc88b93809a986e908ef56b6a97d9216
Nicolas <ni_colasaires@hotmail.com>
2026-03-17 15:51:20 -0300

    feat: Add pytest.ini file with pytest configs. | Add test_same_input_same_output test.

--- shortener/tests.py
+++ shortener/tests.py
@@ -1,2 +1,12 @@
-from django.test import TestCase
+from .services import base62_encode
 
+
+def test_same_input_same_output():
+    link = "Hello, world!"
+    incrementalsim = 1
+
+    combined = (str(incrementalsim) + link).encode("utf-8")
+
+    result = (base62_encode(combined))
+
+    assert result == base62_encode(combined)

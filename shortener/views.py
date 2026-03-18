7f47ad30926c8463a5c1d6ffb2a74eb73aa75106
Nicolas <ni_colasaires@hotmail.com>
2026-03-18 15:44:09 -0300

    chore: Add csrf decorator at shortener/views.py

--- shortener/views.py
+++ shortener/views.py
@@ -5,18 +5,20 @@ from rest_framework import generics
 from shortener.services import base62_encode
 from shortener.models import ShortLink
 from shortener.serializer import ShortLinkSerializer
+from django.views.decorators.csrf import csrf_exempt
 
 
+@csrf_exempt
 class ShortLinkCreateListView(generics.ListCreateAPIView):
     queryset = ShortLink.objects.all()
     serializer_class = ShortLinkSerializer
 
-
+@csrf_exempt
 class ShortlinkRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = ShortLink.objects.all()
     serializer_class = ShortLinkSerializer
 
-
+@csrf_exempt
 class GenerateSurlView(View):
     def get(self, request, pk):
         link = get_object_or_404(ShortLink, pk=pk)

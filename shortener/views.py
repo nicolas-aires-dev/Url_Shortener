eb39cc532a01bbe2ac5893c4ef1315292a0b2c65
Nicolas <ni_colasaires@hotmail.com>
2026-03-18 16:34:03 -0300

    refactor: save() short link index from the object moved from shortener/services.py to shortener/views.py | refactor: apply csrf_exempt correctly on CBVs using method_decorator

--- shortener/views.py
+++ shortener/views.py
@@ -3,25 +3,35 @@ from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 from rest_framework import generics
 from shortener.services import base62_encode
-from shortener.models import ShortLink
+from .models import ShortLink, default_expiration
 from shortener.serializer import ShortLinkSerializer
 from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
 
 
-@csrf_exempt
+@method_decorator(csrf_exempt, name='dispatch')
 class ShortLinkCreateListView(generics.ListCreateAPIView):
     queryset = ShortLink.objects.all()
     serializer_class = ShortLinkSerializer
 
-@csrf_exempt
+@method_decorator(csrf_exempt, name='dispatch')
 class ShortlinkRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = ShortLink.objects.all()
     serializer_class = ShortLinkSerializer
 
-@csrf_exempt
+@method_decorator(csrf_exempt, name='dispatch')
 class GenerateSurlView(View):
     def get(self, request, pk):
+        # pega o objeto pelo ID
         link = get_object_or_404(ShortLink, pk=pk)
+
+        # gera o código curto
         combined = (str(pk) + link.original_link).encode("utf-8")
         surl = base62_encode(combined)[:6]
+
+        # salva no objeto
+        link.shorted_link = surl
+        link.expires_at = default_expiration()
+        link.save()
+
         return JsonResponse({"surl": surl})

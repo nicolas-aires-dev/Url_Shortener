a67bb4c10e7e7cd0c03e012e975a817a71b4c029
Nicolas <ni_colasaires@hotmail.com>
2026-03-19 15:19:45 -0300

    feat: Add redirect-link @shortener/urls.py

--- shortener/urls.py
+++ shortener/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from shortener import views
+from shortener.views import redirect_link
 
 
 urlpatterns = [
     path('', views.ShortLinkCreateListView.as_view(), name='link-create-list'),
     path('<int:pk>/', views.ShortlinkRetrieveUpdateDestroyView.as_view(), name='link-detail-update-delete'),
-    path('<int:pk>/generate-surl/', views.GenerateSurlView.as_view(), name='generate-surl')
+    path('<int:pk>/generate-surl/', views.GenerateSurlView.as_view(), name='generate-surl'),
+    path('<str:shorted_link>', redirect_link, name='redirect-link')
 ]

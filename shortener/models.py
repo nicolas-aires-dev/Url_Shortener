ef7a042858cef325504a00efbaa6a63a7a1f6947
Nicolas <ni_colasaires@hotmail.com>
2026-03-19 14:58:16 -0300

    refactor: Remove post_save signal from model

--- shortener/models.py
+++ shortener/models.py
@@ -1,6 +1,3 @@
-from django.db.models.signals import post_save
-from shortener.models import ShortLink
-from django.dispatch import receiver
 from django.db import models
 from django.utils import timezone
 from datetime import timedelta
@@ -8,7 +5,6 @@ from datetime import timedelta
 
 def default_expiration():
     return timezone.now().date() + timedelta(days=5)
-
 
 class ShortLink(models.Model):
     link_title = models.CharField(null=False, blank=False, max_length=100)
@@ -19,11 +15,6 @@ class ShortLink(models.Model):
     shorted_link = models.CharField(null=True, blank=True, max_length=200)
     clicks = models.IntegerField(default=0)
 
-    @receiver(post_save, sender=ShortLink)
-    def set_surl_created_at(sender, instance, created, **kwargs):
-        if created and instance.shorted_link and not instance.surl_created_at:
-            instance.surl_created_at = timezone.now().date()
-            instance.save()
-
     def __str__(self):
         return self.link_title
+

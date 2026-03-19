af49297780108b41f239fc7488c64080837225b4
Nicolas <ni_colasaires@hotmail.com>
2026-03-19 15:37:32 -0300

    feat: Add link.surl_created_at func @shortener/views

--- shortener/views.py
+++ shortener/views.py
@@ -2,8 +2,9 @@ from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 from shortener.serializer import ShortLinkSerializer
 from .models import ShortLink, default_expiration
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, redirect
 from shortener.services import base62_encode
+from django.utils import timezone
 from rest_framework import generics
 from django.http import JsonResponse
 from django.views import View
@@ -22,16 +23,17 @@ class ShortlinkRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
 @method_decorator(csrf_exempt, name='dispatch')
 class GenerateSurlView(View):
     def get(self, request, pk):
-        # pega o objeto pelo ID
+        # selects object by ID
         link = get_object_or_404(ShortLink, pk=pk)
 
-        # gera o código curto
+        # generate short code
         combined = (str(pk) + link.original_link).encode("utf-8")
         surl = base62_encode(combined)[:6]
 
-        # salva no objeto
+        # saves object
         link.shorted_link = surl
         link.surl_expires_at = default_expiration()
+        link.surl_created_at = timezone.now().date()
         link.save()
 
-        return JsonResponse({"surl": surl})
+        return JsonResponse({"surl": surl, "surl_expires_at": link.surl_expires_at, "surl_created_at": link.surl_created_at})

c8b86ce73039be37d9e095bcc7a5c405cf7fda6e
Nicolas <ni_colasaires@hotmail.com>
2026-03-23 11:16:42 -0300

    refactor: Changes surl_expires_at config to be default=null at a link register. | Add migrations | Removes default_expiration function and imports related.

--- shortener/models.py
+++ shortener/models.py
@@ -1,17 +1,11 @@
 from django.db import models
-from django.utils import timezone
-from datetime import timedelta
-
-
-def default_expiration():
-    return timezone.now().date() + timedelta(days=5)
 
 
 class ShortLink(models.Model):
     link_title = models.CharField(null=False, blank=False, max_length=100)
     created_at = models.DateField(auto_now_add=True)
     surl_created_at = models.DateField(null=True, blank=True)
-    surl_expires_at = models.DateField(default=default_expiration)
+    surl_expires_at = models.DateField(null=True, blank=True)
     original_link = models.CharField(null=False, blank=False, max_length=200)
     shorted_link = models.CharField(null=True, blank=True, max_length=200)
     clicks = models.IntegerField(default=0)

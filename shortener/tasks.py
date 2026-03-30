14be0e1773a2b8b369000b12087587751c41302b
Nicolas <ni_colasaires@hotmail.com>
2026-03-30 13:42:40 -0300

    feat: Add async shared task update_clicks

--- shortener/tasks.py
+++ shortener/tasks.py
@@ -1,10 +1,9 @@
 from celery import shared_task
 from shortener.models import ShortLink
-from django.shortcuts import get_object_or_404
 
 
 @shared_task
-def update_clicks(clicks):
-    surl_clicks = get_object_or_404(ShortLink, clicks=clicks)
-    surl_clicks.clicks += 1
-    surl_clicks.save()
+def update_clicks(link_id):
+    link = ShortLink.objects.get(id=link_id)
+    link.clicks += 1
+    link.save()
